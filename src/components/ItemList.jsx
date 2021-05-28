a9cd4db3664d1760562a9034acd238df86406c21
Emmanuel Tabares <e.tabares20@gmail.com>
2021-05-28 10:44:03 -0300

    intentos de solucion para fetch y json

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,59 +1,50 @@
+/* eslint-disable array-callback-return */
 import React from "react";
 import { useState, useEffect } from "react";
 import Item from "./Item";
 
 const articles = [{
-    id: 1,
-    title: "Notebook Dell I5",
-    description: "lorem ipsum lorem ipsum",
-    stock: 5,
-    price: 110.000
+    "id": 1,
+    "title": "Notebook Dell I5",
+    "description": "lorem ipsum lorem ipsum",
+    "stock": 5,
+    "price": 110.000
 },
 {
-    id: 2,
-    title: "Notebook Dell I7",
-    description: "lorem ipsum lorem ipsum",
-    stock: 10,
-    price: 130.000
+    "id": 2,
+    "title": "Notebook Dell I7",
+    "description": "lorem ipsum lorem ipsum",
+    "stock": 10,
+    "price": 130.000
 },
 {
-    id: 3,
-    title: "Notebook Lenovo I3",
-    description: "lorem ipsum lorem ipsum",
-    stock: 20,
-    price: 125.000
+    "id": 3,
+    "title": "Notebook Lenovo I3",
+    "description": "lorem ipsum lorem ipsum",
+    "stock": 20,
+    "price": 125.000
 },
 {
-    id: 4,
-    title: "Notebook Lenovo I5",
-    description: "lorem ipsum lorem ipsum",
-    stock: 8,
-    price: 140.000
+    "id": 4,
+    "title": "Notebook Lenovo I5",
+    "description": "lorem ipsum lorem ipsum",
+    "stock": 8,
+    "price": 140.000
 }]
 
 export default function ItemList() {
 
 
     const [products, setProducts] = useState([]);
-    
+
     useEffect(() => {
 
-        const task = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(articles);
-            }, 3000)
-        });
-        task.then((data) => {
-            console.log(data)
-            setProducts(data)
-            }
-        )
-
-        task();
-
-    }, []);
-
+        fetch("../data/data.json")
+          .then(response => response.json())
+          .then(data => setProducts(data)
+          );
     
+      }, [])
 
     return (
         <div>
