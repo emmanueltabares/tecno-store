edeaf79edf8b084c547f05f6c13aa5f63a26a3fa
Emmanuel Tabares <e.tabares20@gmail.com>
2021-06-01 22:12:57 -0300

    correcion de consumo de datos json

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,71 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ItemDetail from "./ItemDetail";
 
-const articles = [{
-    id: 1,
-    title: "Notebook Dell I5",
-    description: "lorem ipsum lorem ipsum",
-    stock: 5,
-    price: 110.000
-},
-{
-    id: 2,
-    title: "Notebook Dell I7",
-    description: "lorem ipsum lorem ipsum",
-    stock: 10,
-    price: 130.000
-},
-{
-    id: 3,
-    title: "Notebook Lenovo I3",
-    description: "lorem ipsum lorem ipsum",
-    stock: 20,
-    price: 125.000
-},
-{
-    id: 4,
-    title: "Notebook Lenovo I5",
-    description: "lorem ipsum lorem ipsum",
-    stock: 8,
-    price: 140.000
-}]
+const ItemDetailContainer = () => {
 
-export default function ItemDetailContainer() {
-
-
-    const [products, setProducts] = useState([]);
-    
-    useEffect(() => {
-
-        const task = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(articles);
-            }, 3000)
-        });
-        task.then((data) => {
-            console.log(data)
-            setProducts(data)
-            }
-        )
-
-        task();
-
-    }, []);
-
-    
+    const [detail, setDetails] = useState(null)
 
     return (
-        <div>
-            {products?.map((product) => {
-                <ItemDetail
-                    key={product.id}
-                    title={product.title} 
-                    description={product.description}
-                    stock={product.stock}
-                    price={product.price}
-                />
-            })}
-        </div>
-    )
+
+        <ItemDetail />
+    );
 }
+
+export default ItemDetailContainer;
